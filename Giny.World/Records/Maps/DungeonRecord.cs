cac4134bfc94687c0e7d55bae0ec87b61efe6a3b
Skinz3 <oliviamarius35@gmail.com>
2021-04-03 03:15:36 +0200

    Starting dungeons refactoring.

---
+++ Giny.World/Records/Maps/DungeonRecord.cs
@@ -0,0 +1,69 @@
+﻿using Giny.Core.DesignPattern;
+using Giny.ORM.Attributes;
+using Giny.ORM.Interfaces;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giny.World.Records.Maps
+{
+    [WIP]
+    public class DungeonRecord : ITable
+    {
+        [Primary]
+        public long Id
+        {
+            get;
+            set;
+        }
+        [Update]
+        public string Name
+        {
+            get;
+            set;
+        }
+        [Update]
+        public int EntranceMapId
+        {
+            get;
+            set;
+        }
+        [Update]
+        public int ExitMapId
+        {
+            get;
+            set;
+        }
+
+        [ProtoSerialize]
+        [Update]
+        public List<int> Maps
+        {
+            get;
+            set;
+        }
+
+        [ProtoSerialize]
+        [Update]
+        public List<MonsterRoom> Monsters
+        {
+            get;
+            set;
+        }
+        
+    }
+
+    [ProtoContract]
+    public class MonsterRoom
+    {
+        [ProtoMember(1)]
+        public List<short> MonsterIds
+        {
+            get;
+            set;
+        }
+    }
+}
