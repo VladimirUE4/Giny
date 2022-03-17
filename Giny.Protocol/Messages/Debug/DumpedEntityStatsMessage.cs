d75c02788701ca4cff772f1b823db5a41eb75ce2
Skinz3 <oliviamarius35@gmail.com>
2022-03-17 23:55:29 +0100

    Add missing message in protocol.

---
+++ Giny.Protocol/Messages/Debug/DumpedEntityStatsMessage.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using Giny.Core.Network.Messages;
+using Giny.Protocol.Types;
+using Giny.Core.IO.Interfaces;
+using Giny.Protocol;
+using Giny.Protocol.Enums;
+
+namespace Giny.Protocol.Messages
+{
+    public class DumpedEntityStatsMessage : NetworkMessage
+    {
+        public const ushort Id = 3665;
+        public override ushort MessageId => Id;
+
+        public double actorId;
+        public CharacterCharacteristics stats;
+
+        public DumpedEntityStatsMessage()
+        {
+        }
+        public DumpedEntityStatsMessage(double actorId, CharacterCharacteristics stats)
+        {
+            this.actorId = actorId;
+            this.stats = stats;
+        }
+        public override void Serialize(IDataWriter writer)
+        {
+            if (actorId < -9.00719925474099E+15 || actorId > 9.00719925474099E+15)
+            {
+                throw new Exception("Forbidden value (" + actorId + ") on element actorId.");
+            }
+
+            writer.WriteDouble((double)actorId);
+            stats.Serialize(writer);
+        }
+        public override void Deserialize(IDataReader reader)
+        {
+            actorId = (double)reader.ReadDouble();
+            if (actorId < -9.00719925474099E+15 || actorId > 9.00719925474099E+15)
+            {
+                throw new Exception("Forbidden value (" + actorId + ") on element of DumpedEntityStatsMessage.actorId.");
+            }
+
+            stats = new CharacterCharacteristics();
+            stats.Deserialize(reader);
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
