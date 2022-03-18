42f75cc3b0e5fc8370222725338ce57f8a1abdc7
Skinz3 <oliviamarius35@gmail.com>
2022-03-18 18:00:57 +0100

    Add library.

--- Giny.ProtocolBuilder/Profiles/Profile.cs
+++ Giny.ProtocolBuilder/Profiles/Profile.cs
@@ -4,7 +4,6 @@ using Giny.Core.Extensions;
 using Giny.ProtocolBuilder.Converters;
 using Giny.ProtocolBuilder.Templates;
 using Microsoft.VisualStudio.TextTemplating;
-using Mono.TextTemplating;
 using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
@@ -82,7 +81,7 @@ namespace Giny.ProtocolBuilder.Profiles
             }
             var directoryPath = Path.Combine(OutputDirectory, GetRelativeOutputPath());
 
-            var engine = new TemplatingEngine();
+            var engine = new Engine();
             var host = new TemplateHost(TemplatePath);
 
             var converter = CreateDofusConverter();
